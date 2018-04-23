e6b350a24af7cb8196b16d8edace2cbe43a113ea
vajn <liu___jie@outlook.com>
2018-04-23 14:16:36 +0800

    init project

---
+++ testapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from keystoneauth1.identity import v3
+from keystoneauth1 import session
+from keystoneclient.v3 import client
+
+
+
+# Create your views here.
+def get_keystone(request):
+    auth = v3.Password(auth_url="http://192.168.1.106:5000/v3", username="admin",
+                       password="openstack@good308", project_name="admin",
+                       user_domain_id="default", project_domain_id="default")
+
+    sess = session.Session(auth=auth)
+    keystone = client.Client(session=sess)
+    prj_list = keystone.projects.list()
+
+    prj = prj_list[0]
+
+    return HttpResponse("prj_id:"+prj.id)
+
+
+
